3d2331b65e3d7e6b75b6c4f78c4bd36c83895eb6
zwelden <zach.cw@gmail.com>
2017-05-20 22:16:49 -0500

    converted to classes

--- wiki_sp500_stock_list.py
+++ wiki_sp500_stock_list.py
@@ -21,45 +21,47 @@ import requests
     get_sp_500_holdings() takes the table rows returned from get_sp_500_table as
     an argument and returns a list of dictionaires for each stock holding in the
     format: {'symbol': symbol, 'company_name': company, 'sector': sector}
-
-    get_sp_500() automates the above functions and returns the output from
-    get_sp_500_holdings()
-
 """
-
-
-def get_wiki_page(url=None):
+class ScrapeWikiSP500:
     WIKI = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
     HEADER = {'User-Agent': 'Mozilla/5.0'}
-    page = requests.get(WIKI, headers=HEADER)
-    return page
 
-def soupify(page):
-    soup = BeautifulSoup(page.content, 'lxml')
-    return soup
+    def __init__(self):
+        self.sp_500_holdings = self.get_sp_500_holdings()
 
-def get_sp_500_table(soup):
-    table = soup.find('table', {'class': ['wikitable', 'sortable', 'jquery-tablesorter']})
-    table_rows = table.find_all('tr')
-    return table_rows
+    def get_wiki_page(self):
+        page = requests.get(self.WIKI, headers=self.HEADER)
+        return page
 
-def get_sp_500_holdings(table_rows):
-    sp_500_holdings = []
-    num_symbols = len(table_rows)
-    # in the for loop below, range starts at 1 because index 0 is table header info
-    for i in range(1, num_symbols):
-        index = i
-        row = table_rows[index].find_all('td')
-        symbol = row[0].a.get_text()
-        company = row[1].a.get_text()
-        sector = row[3].get_text()
-        sp_500_holdings.append({'symbol': symbol, 'company_name': company, 'sector': sector})
+    def soupify(self):
+        page = self.get_wiki_page()
+        soup = BeautifulSoup(page.content, 'lxml')
+        return soup
 
-    return sp_500_holdings
+    def get_sp_500_table(self):
+        soup = self.soupify()
+        table = soup.find('table', {'class': ['wikitable', 'sortable', 'jquery-tablesorter']})
+        table_rows = table.find_all('tr')
+        return table_rows
 
-def get_sp_500():
-    page = get_wiki_page()
-    soup = soupify(page)
-    sp_500_table = get_sp_500_table(soup)
-    sp_500_holdings = get_sp_500_holdings(sp_500_table)
-    return sp_500_holdings
+    def get_sp_500_holdings(self):
+        table_rows = self.get_sp_500_table()
+        sp_500_holdings = []
+        num_symbols = len(table_rows)
+        # in the for loop below, range starts at 1 because index 0 is table header info
+        for i in range(1, num_symbols):
+            index = i
+            row = table_rows[index].find_all('td')
+            symbol = row[0].a.get_text()
+            company = row[1].a.get_text()
+            sector = row[3].get_text()
+            sp_500_holdings.append({'symbol': symbol, 'company_name': company, 'sector': sector})
+        return sp_500_holdings
+
+
+
+''' usage:
+sp = ScrapeWikiSP500()
+holdings = sp.sp_500_holdings
+print(holdings)
+'''
--- yahoo_stock_scraper.py
+++ yahoo_stock_scraper.py
@@ -3,86 +3,108 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import time
 
-from wiki_sp500_stock_list import get_sp_500
 
-def get_yahoo_stock_page(symbol):
-    page = requests.get("https://finance.yahoo.com/quote/" + symbol)
-    return page
+class YahooStockQuote:
+    QUOTE_PAGE = "https://finance.yahoo.com/quote/"
 
-def soupify_page(page):
-    soup = BeautifulSoup(page.content, 'html.parser')
-    return soup
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.stock_info = self.get_info()
 
-def get_info_table(soup, side=0):
-    """ side = 0 for left side <-- default
-        side = 1 for right side
-    """
-    quote_summary = soup.find(id="quote-summary")
-    table_div = list(quote_summary.children)[side]
-    table = list(table_div.children)[0]
-    table_body = list(table.children)[0]
-    return table_body
+    def get_yahoo_stock_page(self):
+        page = requests.get(self.QUOTE_PAGE + self.symbol)
+        return page
 
-def get_left_side_info_table(soup):
-    """ uses get_info_table()
-        use to get: previous close, open, bid, ask, day's range,
-            52 week range, volume, avg. volume """
-    table_info = get_info_table(soup, 0)
-    return table_info
+    def soupify_page(self):
+        page = self.get_yahoo_stock_page()
+        soup = BeautifulSoup(page.content, 'html.parser')
+        return soup
 
-def get_right_side_info_table(soup):
-    """ uses get_info_table()
-        use to get: market cap, beta, PE ratio, eps, earnings date,
-            dividend and yeild, ex-dividend date, 1y target est """
-    table_info = get_info_table(soup, 1)
-    return table_info
+    def get_info_table(self, soup, side=0):
+        """ side = 0 for left side <-- default
+            side = 1 for right side
+        """
+        quote_summary = soup.find(id="quote-summary")
+        table_div = list(quote_summary.children)[side]
+        table = list(table_div.children)[0]
+        table_body = list(table.children)[0]
+        return table_body
 
-def get_open_price(info_table):
-    table_open_row = list(info_table.children)[1]
-    stock_open_value = list(table_open_row.children)[1].get_text()
-    price = stock_open_value
-    return price
+    def get_left_side_info_table(self, soup):
+        """ uses get_info_table()
+            use to get: previous close, open, bid, ask, day's range,
+                52 week range, volume, avg. volume """
+        table_info = self.get_info_table(soup, 0)
+        return table_info
 
-def get_info(symbol_list, choice=0):
-    """ takes a stock ticker or list of stock tickers and returns
-        a list of stock ticker: opening value pairs
-        choice list:  <<build choice list>> """
-    # set choice value here
+    def get_right_side_info_table(self, soup):
+        """ uses get_info_table()
+            use to get: market cap, beta, PE ratio, eps, earnings date,
+                dividend and yeild, ex-dividend date, 1y target est """
+        table_info = self.get_info_table(soup, 1)
+        return table_info
 
-    if type(symbol_list) is not list:
-        symbols = list(symbol_list)
-    else:
-        symbols = symbol_list
+    def get_info(self):
 
-    info_list = []
-    for symbol in symbols:
-        page = get_yahoo_stock_page(symbol)
-        soup = soupify_page(page)
-        info_table = get_left_side_info_table(soup)
-        price = get_open_price(info_table) ####
-        info_list.append({symbol: price})
-        time.sleep(.1)
-    return info_list
+        def get_value(table, row):
+            table_row = list(table.children)[row]
+            value = list(table_row.children)[1].get_text()
+            return value
 
+        soup = self.soupify_page()
+        stock_info = {}
 
+        # TODO get date/time
+        # TODO get current price --> after close current price is close price
 
+        '''get left side table info'''
 
-companies = get_sp_500()
-symbol_list = []
-for i in range(5):
-    # symbol = companies[i]["symbol"]
-    # page = requests.get("https://finance.yahoo.com/quote/" + symbol)
-    # soup = BeautifulSoup(page.content, 'html.parser')
-    # quote_summary = soup.find(id="quote-summary")
-    # table_div = list(quote_summary.children)[0]
-    # table = list(table_div.children)[0]
-    # table_body = list(table.children)[0]
-    # table_open_row = list(table_body.children)[1]
-    # stock_open_value = list(table_open_row.children)[1].get_text()
-    # prices[symbol] = stock_open_value
+        ''' replace with:
+        table_left_side = self.get_left_side_info_table(soup)
+        values = ['prev close', 'open price', bid value', ...]
+        for i, value in enumerate(values):
+            stock_info[value] = get_value(table_left_side, i)
+        '''
+        table_left_side = self.get_left_side_info_table(soup)
+        prev_close_value = get_value(table_left_side, 0)
+        stock_info['prev close'] = prev_close_value
+        open_value = get_value(table_left_side, 1)
+        stock_info['open value'] = open_value
+        bid_value = get_value(table_left_side, 2)
+        stock_info['bid value'] = bid_value
+        ask_value = get_value(table_left_side, 3)
+        stock_info['ask value'] = ask_value
+        days_range = get_value(table_left_side, 4)
+        stock_info["day's range"] = days_range
+        fifty_two_week_range = get_value(table_left_side, 5)
+        stock_info['52 week range'] = fifty_two_week_range
+        volume = get_value(table_left_side, 6)
+        stock_info['volume'] = volume
+        avg_volume = get_value(table_left_side, 7)
+        stock_info['avg. volume'] = avg_volume
 
-    # time.sleep(.1)
-    symbol_list.append(companies[i]['symbol'])
+        '''get right side table info'''
+        table_right_side = self.get_right_side_info_table(soup)
+        market_cap = get_value(table_right_side, 0)
+        stock_info['market cap'] = market_cap
+        beta = get_value(table_right_side, 1)
+        stock_info['beta'] = beta
+        pe_ratio = get_value(table_right_side, 2)
+        stock_info['PE ratio'] = pe_ratio
+        eps = get_value(table_right_side, 3)
+        stock_info['EPS'] = eps
+        earnings_date = get_value(table_right_side, 4)
+        stock_info['earnings date'] = earnings_date
+        dividend_and_yield = get_value(table_right_side, 5)
+        stock_info['dividend and yeild'] = dividend_and_yield
+        ex_dividend_date = get_value(table_right_side, 6)
+        stock_info['ex-dividend date'] = ex_dividend_date
+        one_year_target_est = get_value(table_right_side, 7)
+        stock_info['1 yr target est.'] = one_year_target_est
 
-open_price_list = get_info(symbol_list)
-print(open_price_list)
+        return stock_info
+
+''' usage:
+quote = YahooStockQuote('MMM')
+print(quote.symbol, quote.stock_info)
+'''
